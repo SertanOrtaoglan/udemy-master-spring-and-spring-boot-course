537b8feac150d25c4828b66796eee9a62185de6e
Sertan Ortaoğlan <sertanortaoglan@hotmail.com>
2025-01-23 23:51:13 +0300

    Understanding AOP Terminology

--- learn-spring-aop/src/main/java/com/in28minutes/learn_spring_aop/aopexample/aspects/LoggingAspect.java
+++ learn-spring-aop/src/main/java/com/in28minutes/learn_spring_aop/aopexample/aspects/LoggingAspect.java
@@ -46,10 +46,13 @@ public class LoggingAspect {
 	@Before("execution(* com.in28minutes.learn_spring_aop.aopexample.*.*.*(..))")
 	public void logMethodCall(JoinPoint joinPoint) {       
 		//Logic - What?
-		logger.info("Before Aspect - Method is called - {}", joinPoint);
+		logger.info("Before Aspect - {} is called with arguments: {}", joinPoint,joinPoint.getArgs());   //Method parametresi olarak bir 'joinPoint' alıyoruz ve hangi method'ların kesildiğini log'a yazdırıyoruz. 'joinPoint' ayrıca method çağrısı hakkında birkaç ayrıntı daha belirlememize yardımcı olur. Örneğin 'getArgs()'a bakalım.
+		//Kodu bu şekilde çalıştırıp bakarsak log'larda şunları görürüz;   Before Aspect - execution(int com.in28minutes.learn_spring_aop.aopexample.business.BusinessService1.calculateMax()) is called with arguments: []
+		//                                                                 Before Aspect - execution(int[] com.in28minutes.learn_spring_aop.aopexample.data.DataService1.retrieveData()) is called with arguments: []
+		//Her iki log'un son kısmında "is called with arguments: []" ifadesi vardır. Yani kesilen iki method'unda(calculateMax ve retrieveData) şu an için herhangi bir argümanı yoktur demektir. Bu yüzden boş bir dizi [] çıktı olarak ekrana yazdırılır.
 	}
 	
-	//Şimdi kodu çalıştırıp bakarsak; loglarda şunları görürüz;  Before Aspect - Method is called - execution(int com.in28minutes.learn_spring_aop.aopexample.business.BusinessService1.calculateMax())
+	//Şimdi kodu çalıştırıp bakarsak loglarda şunları görürüz;   Before Aspect - Method is called - execution(int com.in28minutes.learn_spring_aop.aopexample.business.BusinessService1.calculateMax())
 	//                                                           Before Aspect - Method is called - execution(int[] com.in28minutes.learn_spring_aop.aopexample.data.DataService1.retrieveData())
 	//Görüldüğü gibi 2 tane "aspect logging" ekrana yazdırıldı. İlk olarak "BusinessService1"in 'calculateMax()' methodu kesildi(engellendi), ardından "DataService1"in 'retrieveData()' methodu kesildi.(engellendi)
 	
